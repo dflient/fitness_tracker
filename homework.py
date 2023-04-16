dcc14ccd2e2b405e655888ec9464f1c475244bd9
Даниил Пастунов <dflient@icloud.com>
2023-04-16 17:26:27 +0300

    Fix bugs

--- homework.py
+++ homework.py
@@ -14,11 +14,13 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        return f'''
+Тип тренировки: {self.training_type};
+Длительность: {self.duration} ч.;
+Дистанция: {self.distance:.3f} км;
+Ср. скорость: {self.speed:.3f} км/ч;
+Потрачено ккал: {self.calories:.3f}.
+'''
 
 
 class Training:
